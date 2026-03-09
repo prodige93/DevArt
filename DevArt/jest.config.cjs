92c3f4eb6188d9c0525209b263b9c1ac0cdbcb6a
eliass <77859813+prodige93@users.noreply.github.com>
2026-03-09 12:43:27 +0100

    Intégration final test Jest

--- DevArt/jest.config.cjs
+++ DevArt/jest.config.cjs
@@ -1,7 +1,13 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'jsdom',
-    setupFilesAfterEnv : ['<rootDir>/jest.setup.ts'],
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
     moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    testMatch: ['**/tests/**/*.test.(tsx)'],
-};
+    testMatch: ['**/tests/**/*.test.(ts|tsx)'],
+    transform: {
+      '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.jest.json' }],
+    },
+    moduleNameMapper: {
+      '\\.(css|less|sass|scss)$': '<rootDir>/tests/mocks/styleMock.js',
+    },
+  };
--- DevArt/tests/Tabs.test.tsx
+++ DevArt/tests/Tabs.test.tsx
@@ -1,15 +1,19 @@
+import { expect, test } from '@jest/globals';
+import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/react';
-import Tabs from '../src/components/Tabs'
+import userEvent from '@testing-library/user-event';
+import Tabs from '../src/components/Tabs';
+import { items } from '../src/App';
 
 test('affiche le bon onglet au clic', async () => {
-    render(<Tabs />);
+    render(<Tabs items={items} />);
+
     const user = userEvent.setup();
-
     // Exemple : cliquer sur un onglet nommé "Onglet 2"
-    await user.click(screen.getByText('Onglet 2'));
-
+    await user.click(screen.getByText('CSS'));
+    
     // Vérifier que le contenu associé apparaît
-    expect(screen.getByText('Onglet 2')).toBeInTheDocument();
+  // @ts-expect-error: matcher ajouté par jest-dom au runtime
+  expect(screen.getByText('CSS')).toBeInTheDocument();
 });
 
